caf68bcf1d619375dabac709910490510dacc2f7
Tshegofatso <106757261+Tshegofatso-Tswai@users.noreply.github.com>
2023-01-15 22:21:10 +0200

    Done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,16 @@
--- Setting MySQL connection
--- To the HBNB
+-- Prepare a MySQL server for the project
+
+-- Create the database 'hbnb_dev_db' if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Create the user 'hbnb_dev' if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the 'hbnb_dev_db' database to the 'hbnb_dev' user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privileges on the 'performance_schema' database to the 'hbnb_dev' user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Flush the privileges to update the changes
+FLUSH PRIVILEGES;
