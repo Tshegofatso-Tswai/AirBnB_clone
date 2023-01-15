6ec6cd41e8e35639089af2df6755b9731e81e72e
Tshegofatso <106757261+Tshegofatso-Tswai@users.noreply.github.com>
2023-01-15 22:23:32 +0200

    Done

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,16 @@
--- Setting MySQL connection
--- To the HBNB
+-- Prepare a MySQL server for the project
+
+-- Create the database 'hbnb_test_db' if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-ALTER USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Create the user 'hbnb_test' if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the 'hbnb_test_db' database to the 'hbnb_test' user
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privileges on the 'performance_schema' database to the 'hbnb_test' user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+-- Flush the privileges to update the changes
+FLUSH PRIVILEGES;
